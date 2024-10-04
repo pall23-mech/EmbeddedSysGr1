af471dcd5287feeb8a71b20c252ab0ee06d955cd
margret96 <margret19@ru.is>
2024-10-04 09:41:33 +0000

    resolve merge conflict

--- DynamicController/include/pwm_control.h
+++ DynamicController/include/pwm_control.h
@@ -4,24 +4,17 @@
 
 #include <Arduino.h> // Include Arduino header for types and macros
 #include "encoder.h" // Include the encoder header for encoder functions
+#include <context.h>
 
-class Context;
+// Declare variables as extern so they are initialized in another file
+extern Encoder encoder;
+extern float targetPPS;
+extern unsigned long lastControlUpdate;
 
-class PwmControl {
-private: 
-    Context* context_; // Pointer to Context
-
-public:
-
-    PwmControl(Context* ctx) : context_(ctx) {}
-
-    // Function to initialize Timer1 for PWM
-    void setupPWM_Timer1();
-    void stopMotor();
-    
-    // Control loop for PWM adjustment
-    void controlLoop();
-    
-};
+// Function to initialize Timer1 for PWM
+void setupPWM_Timer1();
+void stopMotor();
+// Control loop for PWM adjustment
+void controlLoop();
 
 #endif // PWM_CONTROL_H
