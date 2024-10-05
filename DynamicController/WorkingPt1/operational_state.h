eea08ab440cff0e247b56be269dcd50120355875
margret96 <margret19@ru.is>
2024-10-05 19:17:43 +0000

    Fixing problems, checking if every code in the part files work and run as they should

---
+++ DynamicController/WorkingPt1/operational_state.h
@@ -0,0 +1,17 @@
+#ifndef OPERATIONAL_STATE_H
+#define OPERATIONAL_STATE_H
+
+#include <state.h>
+
+class OperationalState : public State
+{
+public:
+    void on_entry() override;
+    void on_exit() override;
+    void on_do() override;
+    
+    void on_reset() override; 
+    void on_transition() override;
+};
+
+#endif // OPERATIONAL_STATE_H
