997202414ac23a568adbaf6ebbda099a1cf21797
margret96 <margret19@ru.is>
2024-10-17 10:09:58 +0000

    Adding a file from Francesca

---
+++ KernelSpEncoderDriver/infinite_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// A infinite loop to fully load the CPU
+
+int main() {
+    // Declare two large floating-point numbers
+    double num1 = 12345678.12345678;
+    double num2 = 87654321.87654321;
+    double result;
+
+    // Infinite loop
+    while (1) {
+        // Multiply the two numbers
+        result = num1 * num2;
+
+    }
+
+    return 0;
+}
