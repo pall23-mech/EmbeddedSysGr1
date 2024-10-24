60359229483ff6ee559824e97f5c66324c29d0b0
margret96 <margret19@ru.is>
2024-10-24 21:16:41 +0000

    Place the code from CodeToTry to src and run it, add #include

--- ContNetComm/src/main.cpp
+++ ContNetComm/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Define the analog pin
+const int analogPin = A0;
+
+// Baud rate for serial communication
+const long baudRate = 9600;
+
+// Interval for reading the analog value (1000 ms = 1 second)
+const unsigned long interval = 1000;
+unsigned long previousMillis = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Start serial communication
+  Serial.begin(baudRate);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Check if it's time to read and send the value
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval) {
+    // Save the current time
+    previousMillis = currentMillis;
+    
+    // Read the analog value
+    int analogValue = analogRead(analogPin);
+    
+    // Print the value to the serial monitor
+    Serial.print("Analog Value: ");
+    Serial.println(analogValue);
+    
+    // Send the value over UART (if needed for other UART communication, add additional code)
+    // Serial.write(analogValue); // Sends the value as a byte (0-255)
+    
+    // If you want to send as a string, uncomment the next line
+    // Serial.println(analogValue);
+  }
 }
