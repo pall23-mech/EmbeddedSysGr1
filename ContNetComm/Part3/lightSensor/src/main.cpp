03c2818f9774f2b37b880126d366012e1cba355a
pall23-mech <142810678+pall23-mech@users.noreply.github.com>
2024-10-31 16:33:24 +0000

    The sensor works, without CRC

--- ContNetComm/Part3/lightSensor/src/main.cpp
+++ ContNetComm/Part3/lightSensor/src/main.cpp
@@ -29,10 +29,10 @@ void loop() {
             if (msg[1] == 0x03) {    // Am I reading?
                 //Serial.write("ABCDEFGH");
                 if (msg[3] == 0) {   // Am I reading from the sensor?
-                    Serial.write("AAAAAAAA");
+                    //Serial.write("AAAAAAAA");
                     _delay_ms(100);
                     if (msg[5] == 1) { // RPi only asked for one value?
-                       // Serial.print("HGFEDCBA");
+                        //Serial.print("HGFEDCBA");
                         data = (uint16_t)(smoothedValue);
                         msg[4] = (uint8_t)(data >> 8);
                         msg[5] = (uint8_t)(data & 0x00FF);
