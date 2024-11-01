661daba446b6a3bcada9a8ad2d164221dcdfc366
pall23-mech <142810678+pall23-mech@users.noreply.github.com>
2024-11-01 16:22:38 +0000

    Lagaði smá

--- ContNetComm/test_part_3/src/main.cpp
+++ ContNetComm/test_part_3/src/main.cpp
@@ -8,6 +8,25 @@ Context *context;
 
 const int FLT_PIN = 2; // Pin for fault signal (modify according to your configuration)
 
+uint16_t ModRTU_CRC(uint8_t buf[], int len)
+{
+    uint16_t crc = 0xFFFF;
+    for (int pos = 0; pos < len; pos++) {
+        crc ^= (uint16_t)buf[pos]; // XOR byte into least sig. byte of crc
+        for (int i = 8; i != 0; i--) { // Loop over each bit
+            if ((crc & 0x0001) != 0) { // If the LSB is set
+                crc >>= 1; // Shift right and XOR 0xA001
+                crc ^= 0xA001;
+            }
+            else {// Else LSB is not set
+                crc >>= 1; // Just shift right
+            }
+        }
+    }
+    // Note, this number has low and high bytes swapped, so use it accordingly (or swap bytes)
+    return crc;
+}
+
 void setup()
 {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps (115200 does not work for Margrét, the output is strange then)
